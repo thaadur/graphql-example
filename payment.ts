a5d11d0da852ad8176470126c9aa598ee68f82a8
thaadur <110046435+thaadur@users.noreply.github.com>
2023-12-21 11:02:02 +0530

    Create payment.ts

---
+++ payment.ts
@@ -0,0 +1,22 @@
+﻿import gql from "graphql-tag";
+export const PAYMENT_TRANSACTION_STATUS = gq1`
+query PaymentTransactionStatus($appointmentId: String!) { 
+paymentTransactionStatus (appointmentId: $appointmentId) { 
+appointment { 
+amountPaid 
+paymentRefId 
+bankTxnId
+displayId
+responseMessage
+paymentDateTime 
+paymentStatus 
+amountBreakup { 
+plansPurchased 
+plansPurchasedwith
+saving_amount
+planValidity
+couponCode
+bookingFee
+}
+}
+}
